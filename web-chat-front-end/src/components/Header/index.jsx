87f1370eedf8516d3d85ffb68e4c18c0f804a345
XDEcat <492697494@qq.com>
2021-04-13 23:55:21 +0800

    顶部导航栏和注册优化

--- web-chat-front-end/src/components/Header/index.jsx
+++ web-chat-front-end/src/components/Header/index.jsx
@@ -1,11 +1,32 @@
 import React, { Component } from 'react'
-
-export default class Header extends Component {
+import { withRouter } from 'react-router-dom';
+import style from './index.module.scss';
+class Header extends Component {
+  //返回
+  handleBack = () => {
+    const { goBack } = this.props.history;
+    goBack(); // 后退
+  }
+  componentDidMount() {
+  }
   render() {
+    let title = "";
+    const { pathname } = this.props.location;
+    switch (pathname) {
+      case "/home":
+        title = "大厅";
+        break;
+      case "/profile":
+        title = "个人中心";
+    }
     return (
       <header>
-        <h2>顶部栏</h2>
+        <h2 className={style.title}>
+          <span className={style.back} onTouchEnd={this.handleBack}></span>
+          {title}
+        </h2>
       </header>
     )
   }
 }
+export default withRouter(Header);
